7c75a4a0fc49ea39b0924f54d85ad7245c799c64
Javier Ferrer <jferrer@ebi.ac.uk>
2025-02-18 08:52:18 +0000

    Show version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,13 @@
 
 import sphinx_rtd_theme  # type: ignore[import] # noqa: F401
 
+import otter
+
 project = 'Otter'
 copyright = '2024, Open Targets Team'  # noqa: A001
 author = 'Open Targets Team'
+version = otter.__version__
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -27,13 +31,11 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_static_path = ['_static']
+html_css_files = ['style.css']
 
+# -- Options for coverage ---------------------------------------------------
 coverage_show_missing_items = True
-
-# Uncomment the following line if you have defined custom style in ./_static/style.css
-# html_css_files = ['style.css']
-
 
 # -- Options for autodoc pydantic -------------------------------------------
 autodoc_pydantic_model_show_json = True
